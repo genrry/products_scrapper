095d0905290512b07ef1e394fd4f0d3c920ec931
genrry <genrryhernandez@gmail.com>
2021-08-02 21:08:10 -0500

    removed unused import and not specifying chromedriver path

--- scrapper.py
+++ scrapper.py
@@ -11,7 +11,10 @@ def access_page():
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--no-sandbox")
     options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=options)
+    driver = webdriver.Chrome(
+        # executable_path=os.environ.get("CHROMEDRIVER_PATH"), 
+        options=options
+    )
     driver.get("https://www.rappi.com.co/login")
     driver.implicitly_wait(20)
     return driver
